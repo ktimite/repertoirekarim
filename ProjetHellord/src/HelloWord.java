a1ae8a22e860d3e351689ec237579e41d7a537ef
Karim <Karim@pc-karim>
2013-09-07 12:03:22 +0100

    Première validation

---
+++ ProjetHellord/src/HelloWord.java
@@ -0,0 +1,17 @@
+
+public class HelloWord {
+
+	public HelloWord() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Good morning Miage i'm Timité");
+
+	}
+
+}
