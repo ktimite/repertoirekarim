a829d07670c6322e41fdb5cc3c406e7a67ff6957
Karim <Karim@pc-karim>
2013-09-07 12:16:05 +0100

    deuxième validation

---
+++ ProjetHellord/src/ClassB.java
@@ -0,0 +1,16 @@
+
+public class ClassB {
+
+	public ClassB() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Classe b");
+	}
+
+}
